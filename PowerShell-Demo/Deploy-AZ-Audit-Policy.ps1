775626e3a0e3da3b112f23b694ef68c2bc784920
jf781 <50719731+jf781@users.noreply.github.com>
2020-09-09 11:46:02 -0500

    Updated parameters in PS script to deploy Policy Def and Assignment

--- PowerShell-Demo/Deploy-AZ-Audit-Policy.ps1
+++ PowerShell-Demo/Deploy-AZ-Audit-Policy.ps1
@@ -1,27 +1,33 @@
 
-#Define the tags that you wish to check for 
-$Tag1 = "costcenter"
-$Tag2 = "owner"
+# Define the tags that you wish to check for 
+$tag1 = "CostCenter"
+$tag2 = "Owner"
+
+# Define Policy Definition and Assingment Names/Descriptions
+$defName = "Audit Resource Tags"
+$defDesc = "Audits all resources to ensure necessary tags are created"
+$assignName = "Audit Resource Tags - $tag1 and $tag2"
+$assignDesc = "Audits all applicable resources to verify if the tags '$tag1' and '$tag2' are present"
 
 # Creates the Policy Definition
-$Definitions = New-AzPolicyDefinition -Name "Audit Resource Tags" `
--Description "Audits all resources to ensure necessary tags are created" `
+$definitions = New-AzPolicyDefinition -Name $defName `
+-Description $defDesc `
 -Mode Indexed `
--Policy https://raw.githubusercontent.com/jf781/AzPub/master/AzurePolicy/Audit/AZPolicy-Audit-Tag-Name.json `
--Parameter https://raw.githubusercontent.com/jf781/AzPub/master/AzurePolicy/Audit/AZPolicy-Audit-Tag-Name-Parameters.json
+-Policy https://raw.githubusercontent.com/jf781/Azure.Policy.Demos/master/PowerShell-Demo/AZPolicy-Audit-Tag-Name.json `
+-Parameter https://raw.githubusercontent.com/jf781/Azure.Policy.Demos/master/PowerShell-Demo/AZPolicy-Audit-Tag-Name-Parameters.json
 
 # Defines the Required resource tags your auditing
-$PolicyParameters = @{tagName1=$Tag1;tagName2=$Tag2}
+$policyParameters = @{tagName1=$tag1;tagName2=$tag2}
 
 # Defines the current subscription as the scope for the policy
-$Sub = "/subscriptions/" + (Get-AzSubscription).SubscriptionId
+$sub = "/subscriptions/" + (Get-AzContext).Subscription.Id
 
 # Assigns the policy to the current subscription
-New-AzPolicyAssignment -Name "Audit Resource Tags" `
--PolicyDefinition $Definitions `
--Description "Checks for the tags defined in the 'Parameters' section and then audits all applicable resources to verify if those tags are present" `
--Scope $Sub `
--PolicyParameterObject $PolicyParameters
+New-AzPolicyAssignment -Name $assignName `
+-PolicyDefinition $definitions `
+-Description $assignDesc `
+-Scope $sub `
+-PolicyParameterObject $policyParameters
 
 
 
